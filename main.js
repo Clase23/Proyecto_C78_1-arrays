916e0f164cc371289b5e34e7b5446d88826c6d51
Clase23 <149850204+Clase23@users.noreply.github.com>
2025-03-01 17:37:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+ //Cómo declarar un array. El id para referirnos al innerHTML es declarar_arrays
+ 
+ //Cómo obtener el primer elemento de un array. El id para referirnos al innerHTML es acceder_primer_elemento
+
+ //Cómo obtener el segundo elemento de un array. El id para referirnos al innerHTML es acceder_segundo_elemento
+ 
+ //Cómo obtener el tercer elemento de un array. El id para referirnos al innerHTML es acceder_tercer_elemento
+
+ //Cómo obtener la longitud del array. El id para referirnos al innerHTML es ver_longitud_del_array
+
+ //Cómo crear un bucle del array. El id para referirnos al innerHTML es bucle_del_array
+
+ //Añadir un elemento al final de un array con el método push
+ //Cómo añadir un elemento al final de un array. El id para referirnos al innerHTML es añadir_elemento_al_final_de_un_array
+ 
+ //Cómo añadir más elementos al final de un array. El id para referirnos al innerHTML es añadir_más_elementos_al_final_de_un_array
+ 
+ //Cómo declarar un array con el alfabeto. El id para referirnos al innerHTML es ver_array_alfabeto
+ 
+ //Cómo ordenar el array del alfabeto. El id para referirnos al innerHTML es ver_array_alfabeto_ordenado
+ 
+ //Cómo invertir el array. El id para referirnos al innerHTML es ver_array_invertido
+
+ //Cómo declarar un array con números. El id para referirnos al innerHTML es ver_array_num
+
+ //Cómo declarar un número con valor máximo del array con números. El id para referirnos al innerHTML es ver_num_max
+
+ //Cómo encontrar el número con valor mínimo del array con números. El id para referirnos al innerHTML es ver_num_min
+
+
+//"ACTIVIDAD ADICIONAL"
+
+ //Añadir un elemento al inicio de un array con el método unshift
+ //Cómo añadir un elemento al inicio de un array. El id para referirnos al innerHTML es añadir_elemento_al_inicio_del_array
+ 
+ //Cómo agregar más elementos al inicio de la matriz? El id para referirnos al innerHTML es añadir_más_elementos_al_inicio_del_array
+ 
+ //Cómo remover un elemento al inicio de un array. El id para referirnos al innerHTML es remover_elemento_al_inicio_del_array
+ 
+ //Cómo remover un elemento al final de un array. El id para referirnos al innerHTML es remover_elemento_al_final_del_array
+ 
+ //Cómo declarar un array con doce estados de México
+
+ //Cómo declarar un array con otros doce estados de México
+
+ //Cómo unir dos arrays. El id para referirnos al innerHTML es unir_dos_arrays
